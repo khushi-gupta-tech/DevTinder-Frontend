5a741d54cded9b27b511acda9933845297040541
khushi gupta <kg834208@gmail.com>
2025-11-18 21:33:38 +0530

    set up routing

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # DevTinder
 
 - Create a Vite + React application
+
+
+# Planning
+
+Body 
+  NavBar 
+  Route=/ =>Feed
+  Route=/login => Login
+  Route=/connection
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import NavBar from "./components/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <NavBar/>
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
