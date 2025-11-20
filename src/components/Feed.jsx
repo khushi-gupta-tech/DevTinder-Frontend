c0d861b9a70fd65b9d6a7c415a7b3cd61f4458a1
khushi gupta <kg834208@gmail.com>
2025-11-21 00:42:06 +0530

    create feed slice

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import axios from 'axios';
+import {BASE_URL} from "../constants";
 
 const Feed = () => {
+  const getFeed = async()=>{
+    try{
+      const res = await axios.get(BASE_URL+"/feed");
+     
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div>Feed</div>
   )
