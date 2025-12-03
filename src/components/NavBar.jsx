2aeed26f95f0253826f9407fde3acbe8d4af8bc2
khushi gupta <kg834208@gmail.com>
2025-12-03 22:25:15 +0530

    fix the navbar make responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -54,8 +54,8 @@ const NavBar = () => {
       <div className="flex ml-auto items-center gap-5">
         {user && (
           <div className="dropdown dropdown-end flex items-center gap-3">
-            <h1>{user.isPremium ? "✅":"" }</h1>
-            <p className=" text-sm opacity-90">
+            <h1 className="hidden sm:flex">{user.isPremium ? "✅":"" }</h1>
+            <p className=" hidden sm:flex text-sm opacity-90">
               Welcome, <span className="font-semibold">{user.firstName}</span>
             </p>
 
